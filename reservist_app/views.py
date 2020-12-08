c47cfad9896a82fa35b530dd3897504681194851
Eagleman <warrenching33@gmai.com>
2020-12-09 07:39:32 +0800

    first commit

---
+++ reservist_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import News
+from .serializers import NewsSerializer
+
+
+class NewsView(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
